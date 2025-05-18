a301935634c5f456b91f4783c943068cdda3b3b1
TTTTina33487 <tinatang20241127@gmail.com>
2025-05-18 18:09:54 +0800

    Update library_card.java

--- class/library_card.java
+++ class/library_card.java
@@ -1,18 +1,65 @@
-package my_library;
+package my_library.Entity;
 
-public class library_card {
-	private int periodOfValidity;
-public library_card() {
-	
-}
-public library_card(int periodOfValidity){
-	this.periodOfValidity = periodOfValidity;
-}
-	public int getPeriodOfValidity() {
-		return periodOfValidity;
-	}
+import java.util.ArrayList;
+import java.util.List;
 
-	public void setPeriodOfValidity(int periodOfValidity) {
-		this.periodOfValidity = periodOfValidity;
-	}
+public class LibraryCard {
+    private int periodOfValidity;
+
+    private List<BorrowedBook> borrowedBooks;
+    private List<ReservationBook> reservationBooks;
+
+    public LibraryCard() {
+        this.borrowedBooks = new ArrayList<>();
+        this.reservationBooks = new ArrayList<>();
+    }
+
+    public LibraryCard(int periodOfValidity) {
+        this.periodOfValidity = periodOfValidity;
+        this.borrowedBooks = new ArrayList<>();
+        this.reservationBooks = new ArrayList<>();
+    }
+
+    public int getPeriodOfValidity() {
+        return periodOfValidity;
+    }
+
+    public void setPeriodOfValidity(int periodOfValidity) {
+        this.periodOfValidity = periodOfValidity;
+    }
+
+    public List<BorrowedBook> getBorrowedBooks() {
+        return borrowedBooks;
+    }
+
+    public void setBorrowedBooks(List<BorrowedBook> borrowedBooks) {
+        this.borrowedBooks = borrowedBooks;
+    }
+
+    public List<ReservationBook> getReservationBooks() {
+        return reservationBooks;
+    }
+
+    public void setReservationBooks(List<ReservationBook> reservationBooks) {
+        this.reservationBooks = reservationBooks;
+    }
+
+    @Override
+    public String toString() {
+        return "LibraryCard{" +
+                "periodOfValidity=" + periodOfValidity +
+                ", borrowedBooks=" + borrowedBooks +
+                ", reservationBooks=" + reservationBooks +
+                '}';
+    }
+
+    // 可添加类型区分方法
+    public String getCardType() {
+        return "Generic";
+    }
+
+    public boolean canRenew() {
+        return false;
+    }
 }
+
